7f6ce5443dcca04a8bd78f133da2e87ddd23e59c
pranitaaeer <aeerpranita@gmail.com>
2026-08-01 16:58:16 +0530

    add middleware

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,7 +4,8 @@ const cors = require("cors")
 
 const app = express()
 
-app.use(express.json())
+app.use(express.json({ limit: "10mb" }))
+app.use(express.urlencoded({ extended: true, limit: "10mb" }))
 app.use(cookieParser())
 app.use(cors({
     origin: "https://interview-ai-umber-one.vercel.app",
